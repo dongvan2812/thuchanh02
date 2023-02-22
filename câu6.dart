fcce2bc03a9c4283a33683714519fdfd6a4a21c1
ChuaTeDongVan <86394@st.vimaru.edu.vn>
2023-02-22 10:52:56 +0700

    Add new project thuchanh2

---
+++ câu6.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(List<String> args) {
+  int a = 5;
+  int b = 10;
+  for (int i = 1; i <= b; i++) {
+    int c = a * i;
+    print("$a x $i = $c");
+  }
+}
---
+++ câu8.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+void main() {
+  print('Máy tính đơn giản');
+
+  stdout.write('Nhập số thứ nhất: ');
+  var num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Nhập số thứ hai: ');
+  var num2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Nhập phép tính (+, -, *, /): ');
+  var op = stdin.readLineSync()!;
+
+  var result;
+  
+    if (op == '+') {
+      result = num1 + num2;
+    } 
+else if (op == '-') 
+{
+    result = num1 - num2;
+    } 
+else if (op == '*') {
+result = num1 * num2;
+    } 
+else if (op == '/') {
+if (num2 == 0) {
+ throw('Không thể chia cho 0');
+      }
+      result = num1 / num2;
+    } else 
+{
+      print('Phép tính không hợp lệ.');
+      return;
+    }
+    print('Kết quả: $result');
+  } 
